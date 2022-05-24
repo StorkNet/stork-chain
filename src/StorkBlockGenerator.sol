1c49b6d336fb10b862fe92efa7f0ddf999b5dcd0
Shankar <shankars0899@gmail.com>
2022-05-25 01:57:36 +0530

    added the time window for block creation

--- src/StorkBlockGenerator.sol
+++ src/StorkBlockGenerator.sol
@@ -7,8 +7,8 @@ contract StorkBlockGenerator is StorkBlock {
     struct TxData {
         address[] validators;
         uint256 storkId;
-        Stork stork;
-        StorkParameter storkParameter;
+        bytes stork;
+        bytes storkParameter;
         string fallbackFunction;
         bool hasStork;
         bool hasParameter;
@@ -22,11 +22,14 @@ contract StorkBlockGenerator is StorkBlock {
     }
 
     mapping(address => uint256) public clientCounter;
-    mapping(bytes32 => TxData) public txData;
+
+    // index input for txData
     bytes32[] public txHashes;
+    mapping(bytes32 => TxData) public txData;
 
     uint256 public txCount;
     address[] public validators;
+
     mapping(address => ValidatorInfo) public validatorInfo;
 
     constructor(uint256 _blockLockDuration) {
@@ -39,10 +42,17 @@ contract StorkBlockGenerator is StorkBlock {
         address _clientAddr,
         string calldata _queryName,
         uint256 _storkId,
-        Stork calldata _txStork,
-        StorkParameter calldata _txStorkParameter,
+        bytes calldata _txStork,
+        bytes calldata _txStorkParameter,
         string calldata fallbackFunction
     ) external isNotLocked(true) {
+        if (blockCreateDuration < block.timestamp) {
+            addTxToBlock();
+            setNextBlockLockTime();
+            blockCount++;
+        }
+
+
         bytes32 txHashed = keccak256(
             abi.encode(
                 _clientAddr,
@@ -52,11 +62,11 @@ contract StorkBlockGenerator is StorkBlock {
                 _txStorkParameter
             )
         );
-        txHashes.push(txHashed);
         // if the txHash doesn't exist, add it to the TxList and increase the txCount of the client
         if (!txData[txHashed].isProposed) {
             clientCounter[_clientAddr] += queryInfo[_queryName].cost;
             txData[txHashed].isProposed = true;
+            txHashes.push(txHashed);
         }
 
         if (queryInfo[_queryName].hasStork) {
@@ -77,20 +87,22 @@ contract StorkBlockGenerator is StorkBlock {
             txData[txHashed].validators.push(msg.sender);
             validatorInfo[msg.sender].txCount += queryInfo[_queryName].cost;
         }
-        //store the tx data
-        // txData[txHashed].stork = _txStork;
-        // txData[txHashed].stork = _txStork;
 
         // this creates the list of unique validators
         if (!validatorInfo[msg.sender].isAdded) {
             validators.push(msg.sender);
         }
     }
-}
 
-// 0x364C5DA8CF1B73FB53A2BEdBcfb07190CD814d6c
-// createStork
-// 0
-// [0,0,"0x7368616e6b6172000000000000000000000000000000000000000000000000"]
-// [1,1,"hi"]
-// deeznuts
+    function addTxToBlock() internal {
+        blocks[blockCount].isSealed = true;
+        for (uint8 i; i < txHashes.length; ++i) {
+            if (
+                txData[txHashes[i]].validators.length >=
+                validators.length * percentageToPass
+            ) {
+                blocks[blockCount].txHash.push(txHashes[i]);
+            }
+        }
+    }
+}
