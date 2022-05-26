98a7801ed44fe4021fc4e4e03c59378852b06515
Shankar <shankars0899@gmail.com>
2022-05-26 17:14:12 +0530

    test for integration

--- src/OraclePoSt.sol
+++ src/OraclePoSt.sol
@@ -1,50 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract OraclePoSt {
+import "./PoH.sol";
+
+contract OraclePoSt is PoH {
+    address[] private blockValidators;
+    uint256 public freqSum;
+
     struct PoSt {
-        uint256 validatorAddr;
+        address validatorAddr;
         uint256 validatorFreq;
         uint256 validatorFreqBackup;
     }
-
-    uint256[] private jobMiners;
-    uint256 public sum;
     PoSt[] public postValidators;
 
     constructor() {
-        validatorPoH(4, 2);
-        validatorPoH(3, 1);
-        validatorPoH(8, 3);
-        validatorPoH(12, 1);
-        validatorPoH(2, 2);
-        validatorPoH(6, 3);
-        validatorPoH(10, 3);
-        validatorPoH(11, 1);
-        validatorPoH(1, 2);
-        validatorPoH(9, 1);
+        initValidatorPoH();
     }
 
-    function getMinerFrequency(uint256 _miner)
-        public
-        view
-        returns (PoSt memory)
-    {
-        return postValidators[_miner];
+    function startPoSt(
+        uint256 _key,
+        uint8 _validatorsRequired,
+        address[] calldata validators
+    ) external {
+        for (uint256 i = postValidators.length; i > 0; i--) {
+            postValidators.pop();
+        }
+
+        for (uint256 i; i < validators.length; ++i) {
+            uint256 validatorPower = poh[validators[i]].validatorFreqBackup;
+            freqSum += validatorPower;
+            postValidators.push(
+                PoSt(validators[i], validatorPower, validatorPower)
+            );
+        }
+        findMiner(_key, _validatorsRequired);
     }
 
-    function validatorPoH(uint256 _address, uint256 _freq) internal {
-        postValidators.push(PoSt(_address, _freq, _freq));
-        sum += _freq;
-    }
-
-    function findMiner(uint256 key, uint256 _size) public {
+    function findMiner(uint256 key, uint256 _size) internal {
         uint256 validatorCount = postValidators.length;
         uint256 loc = 0;
         uint256 freqCounter = 0;
         uint256 nextValidator = 0;
         for (uint256 i = 0; i < _size; i++) {
-            loc = (((loc + (key++ % _size)) % validatorCount) + 1) % sum;
+            loc =
+                ((((loc + (key++ % _size)) % validatorCount) + 1) % freqSum) +
+                1;
             freqCounter = 0;
             uint256 indexCounter = 0;
             while (freqCounter < loc) {
@@ -59,12 +60,12 @@ contract OraclePoSt {
                 }
                 indexCounter++;
             }
-            jobMiners.push(
+            blockValidators.push(
                 postValidators[
                     (nextValidator + indexCounter - 1) % validatorCount
                 ].validatorAddr
             );
-            sum -= postValidators[
+            freqSum -= postValidators[
                 (nextValidator + indexCounter - 1) % validatorCount
             ].validatorFreq;
             postValidators[(nextValidator + indexCounter - 1) % validatorCount]
@@ -73,7 +74,7 @@ contract OraclePoSt {
         }
     }
 
-    function retMiners() public view returns (uint256[] memory) {
-        return jobMiners;
+    function getBlockValidators() public view returns (address[] memory) {
+        return blockValidators;
     }
 }
