74dacb320e8358bc3a8ab61961f90faf725fbf39
Shankar <shankars0899@gmail.com>
2022-06-05 08:01:07 +0530

    uses keys as generated for geth

--- src/PoH.sol
+++ src/PoH.sol
@@ -11,17 +11,15 @@ contract PoH {
     mapping(address => ValidatorPoH) public poh;
 
     function initValidatorPoH() public {
-        poh[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4]=ValidatorPoH(3,3);
-        poh[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2]=ValidatorPoH(1,1);
-        poh[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db]=ValidatorPoH(1,1);
-        poh[0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB]=ValidatorPoH(2,2);
-        poh[0x617F2E2fD72FD9D5503197092aC168c91465E7f2]=ValidatorPoH(1,1);
-        poh[0x17F6AD8Ef982297579C203069C1DbfFE4348c372]=ValidatorPoH(3,3);
-        poh[0x5c6B0f7Bf3E7ce046039Bd8FABdfD3f9F5021678]=ValidatorPoH(1,1);
-        poh[0x03C6FcED478cBbC9a4FAB34eF9f40767739D1Ff7]=ValidatorPoH(1,1);
-        poh[0x1aE0EA34a72D944a8C7603FfB3eC30a6669E454C]=ValidatorPoH(2,2);
-        poh[0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC]=ValidatorPoH(2,2);
-        poh[0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c]=ValidatorPoH(1,1);
-        poh[0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C]=ValidatorPoH(1,1);
+        poh[0x073b53769a3CdbdD9C6cE24fEb87EB366e8C607C]=ValidatorPoH(3,3);
+        poh[0x39F1Bde194d7ef4b4F4c1C1dFcD6a9295225437B]=ValidatorPoH(1,1);
+        poh[0x35B272Ae597d0e02dde458ae0dDbACcc07e7F8d2]=ValidatorPoH(1,1);
+        poh[0x1A8376C671543FFE5ed0c86F4E71C710ADaD1988]=ValidatorPoH(2,2);
+        poh[0x7129Fc802deee39747a1571C88Ad51169EB60798]=ValidatorPoH(1,1);
+        poh[0xA42eaA8fA6c3123E464E1D74e1629FDB49e03412]=ValidatorPoH(3,3);
+        poh[0xefa10aa4CcE623616D398cc468B313e7a548bCd2]=ValidatorPoH(1,1);
+        poh[0x53B07e381c519B31c96E716F552cDe72CDfD3c2c]=ValidatorPoH(1,1);
+        poh[0x8fB056B94c69179a3759f54e071E2666Fbe3F8E5]=ValidatorPoH(2,2);
+        poh[0xb9ffd4980F02358605f78F33929458f55a1d4A73]=ValidatorPoH(2,2);
     }
 }
