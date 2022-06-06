e801a9e0627b57f5cf5d9bd0437257b4ba0981e8
Shankar <shankars0899@gmail.com>
2022-06-06 09:15:45 +0530

    added create, request, and delete

--- src/StorkDataStore.sol
+++ src/StorkDataStore.sol
@@ -1,32 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract StorkDataStore {
-    mapping(address => mapping(string => mapping(uint256 => bytes)))
+import "./StorkTypes.sol";
+
+contract StorkDataStore is StorkTypes {
+    modifier IsOnlyStorkBlockGenerator() {
+        require(
+            msg.sender == storkBlockGeneratorAddress,
+            "is not stork block generator"
+        );
+        _;
+    }
+
+    address storkBlockGeneratorAddress;
+    mapping(address => mapping(bytes32 => mapping(uint8 => bytes)))
         public dataStore;
+    mapping(address => mapping(bytes32 => bytes)) public phalanx;
+
+    constructor(address _storkBlockGeneratorAddress) {
+        storkBlockGeneratorAddress = _storkBlockGeneratorAddress;
+    }
+
+    function createNewPhalanx(
+        address _addr,
+        bytes32 _phalanxName,
+        bytes calldata _phalanxData
+    ) public {
+        phalanx[_addr][_phalanxName] = _phalanxData;
+    }
 
     function createNewData(
         address _addr,
-        string calldata _storkType,
-        uint256 _storkId,
+        bytes32 _phalanxName,
+        uint8 _storkId,
         bytes calldata _storkData
     ) public {
-        dataStore[_addr][_storkType][_storkId] = _storkData;
+        dataStore[_addr][_phalanxName][_storkId] = _storkData;
     }
 
     function readData(
         address _addr,
-        string calldata _storkType,
-        uint256 _storkId
+        bytes32 _phalanxName,
+        uint8 _storkId
     ) public view returns (bytes memory) {
-        return (dataStore[_addr][_storkType][_storkId]);
+        return (dataStore[_addr][_phalanxName][_storkId]);
     }
 
     function deleteData(
         address _addr,
-        string calldata _storkType,
-        uint256 _storkId
+        bytes32 _phalanxName,
+        uint8 _storkId
     ) public {
-        delete dataStore[_addr][_storkType][_storkId];
+        delete dataStore[_addr][_phalanxName][_storkId];
     }
 }
